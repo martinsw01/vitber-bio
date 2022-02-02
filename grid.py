cecd1e20782e99ec8e623336c8f942f8c2d31206
martinsw01 <martin.s.winther@gmail.com>
2022-02-02 18:56:59 +0100

    Initialize functions operating on the grid

---
+++ grid.py
@@ -0,0 +1,18 @@
+def create_grid(N):
+    pass
+
+
+def choose_random_polymer(grid):
+    pass
+
+
+def choose_random_direction(grid):
+    pass
+
+
+def is_illegal(grid, polymer, direction):
+    pass
+
+
+def move_polymer(grid, polymer, direction):
+    pass
