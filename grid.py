33671f4e92594c1713b2b4d7f82f287db2d7ef3d
martinsw01 <martin.s.winther@gmail.com>
2022-02-04 19:20:21 +0100

    Implemented functions operating on grid. Not tested yet!

--- grid.py
+++ grid.py
@@ -1,18 +1,30 @@
-def choose_random_polymer(grid):
-    pass
+import numpy as np
 
 
-def choose_random_direction(grid):
-    pass
+def choose_random_polymer(M):
+    return np.random.randint(-M, M)
 
 
-def is_illegal(grid, polymer, direction):
-    pass
+directions = np.array([[-1, 0], [0, -1], [1, 0], [0, 1]])
+
+
+def choose_random_direction():
+    i = np.random.randint(0, 3)
+    return directions[i]
 
 
 def is_illegal_move(grid, polymer, direction):
-    pass
+    N = len(grid)
+    polymer_indices = np.where(grid == polymer)
+    new_polymer_indices = tuple(((np.array(polymer_indices).T + direction) % N).T)
+    is_solvent = grid[new_polymer_indices] == 0
+    is_same_polymer = grid[new_polymer_indices] == polymer
+    return np.logical_or(is_solvent, is_same_polymer).all()
 
 
 def move_polymer(grid, polymer, direction):
-    pass
+    N = len(grid)
+    polymer_indices = np.where(grid == polymer)
+    new_polymer_indices = tuple(((np.array(polymer_indices).T + direction) % N).T)
+    grid[polymer_indices] = 0
+    grid[new_polymer_indices] = polymer
