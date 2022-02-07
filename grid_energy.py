021600b2cbf65b78f5e6762b11fd3a834a2198a4
martinsw01 <martin.s.winther@gmail.com>
2022-02-07 16:06:56 +0100

    Closes #4 1c) Implemented function calculating grid energy as described on page 6 with parameters from page 12

---
+++ grid_energy.py
@@ -0,0 +1,25 @@
+import numpy as np
+from grid import neighbour_indices
+
+ε_r = 78  # [1] Relative permittivity
+ε_0 = 8.85418782e-12  # vacuum permittivity[m^-3 kg^-1 s^4 A^2]
+a = 23e-6  # Distance between grid points (23 microns)
+e = 1.60217662e-19  # [C]
+α = e ** 2 / (4 * np.pi * ε_r * ε_0) * a ** 2
+
+
+# 1 d)
+def calc_energy(grid):
+    N = len(grid)
+    monomer_indices = np.where(grid != 0)
+    relative_energy = 0
+
+    for monomer_index in zip(*monomer_indices):
+        monomer = grid[monomer_index]
+        for i, j in neighbour_indices(monomer_index):
+            neighbour_monomer = grid[i % N, j % N]
+            if neighbour_monomer != monomer and neighbour_monomer != 0:  # i.e. not the same type or solvent
+                some_energy = np.sign(monomer) * np.sign(neighbour_monomer)
+                relative_energy += some_energy
+
+    return relative_energy * α**2 / 2
