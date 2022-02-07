fd1cfe46624ea367a63ca71b3b276c388c639a7c
martinsw01 <martin.s.winther@gmail.com>
2022-02-07 16:10:13 +0100

    Moved old files into its own package

--- old/old_main.py
+++ old/old_main.py
@@ -1,15 +1,15 @@
 import matplotlib.pyplot as plt
 
-from montecarlo import monte_carlo
-from grid_creator import create_grid
+from old_montecarlo import monte_carlo
+from old_grid_creator import create_grid
 from visualization import create_plotter
 
 
 def main():
-    grid_parameters = 20, 10, 5
+    grid_parameters = 15, 25, 1
     grid = create_grid(*grid_parameters)
-    # final_grid, mean_grid, energy_profile = monte_carlo(grid, grid_parameters, t_equil=1000, t_r=10, n=10)
-    final_grid, mean_grid, energy_profile = monte_carlo(grid, grid_parameters, t_equil=1000, t_r=10, n=10,
+    # final_grid, mean_grid, energy_profile = monte_carlo(grid, grid_parameters, t_equil=50_000, t_r=10, n=10)
+    final_grid, mean_grid, energy_profile = monte_carlo(grid, grid_parameters, t_equil=50_000, t_r=10, n=10,
                                                         on_iteration=create_plotter())
     _, (ax1, ax2, ax3) = plt.subplots(3)
     ax1.pcolormesh(final_grid)
