70c00aa6867cc64c2257980232fd295b8d9221b7
martinsw01 <martin.s.winther@gmail.com>
2022-02-08 12:53:11 +0100

    Fixed bug in neighbor_coordinates. Did not return neighbor to the right. Instead, it returned two instances of the left neighbor index

--- grid.py
+++ grid.py
@@ -2,7 +2,7 @@ import numpy as np
 
 
 def neighbor_coordinates(N, i, j):
-    neighbors = (((i - 1) % N, j), (i, (j - 1) % N), ((i + 1) % N, j), (i, (j - 1) % N))
+    neighbors = (((i - 1) % N, j), (i, (j - 1) % N), ((i + 1) % N, j), (i, (j + 1) % N))
     return neighbors
 
 
