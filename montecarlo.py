5da4a1fe6acc56656e43132330079dd430c5ab1b
martinsw01 <martin.s.winther@gmail.com>
2022-02-08 13:23:32 +0100

    Made it possible to plot animations to see the development of the grid

--- montecarlo.py
+++ montecarlo.py
@@ -1,17 +1,20 @@
-import numpy as np
-from grid_energy import calc_energy
 import random
+
+import numpy as np
+
 from grid import choose_random_direction, choose_random_monomer, illegal_move, move_monomer
+from grid_energy import calc_energy
 
 
+def do_nothing(*args): pass
 
-#1 e)
-def monte_carlo(grid, N_s, M, T):
 
+# 1 e)
+def monte_carlo(grid, N_s, M, T, on_iteration=do_nothing):
     epsilon = np.zeros(N_s)
     energy = calc_energy(grid)
     epsilon[0] = energy
-    beta = 1/(T*1.380649e-23)
+    beta = 1 / (T * 1.380649e-23)
 
     for i in range(N_s):
         energy = calc_energy(grid)
@@ -23,13 +26,13 @@ def monte_carlo(grid, N_s, M, T):
         else:
             new_grid = move_monomer(grid, rand_monomer, rand_direction)
             new_energy = calc_energy(new_grid)
-            if new_energy < energy or random.uniform(0, 1) < np.exp(-beta*(new_energy-energy)):
+            if new_energy < energy or random.uniform(0, 1) < np.exp(-beta * (new_energy - energy)):
                 grid = new_grid
                 energy = new_energy
 
         epsilon[i] = energy
 
+        if i % 50 == 0:
+            on_iteration(grid, epsilon[:i])
+
     return grid, epsilon
-
-
-
--- simulations.py
+++ simulations.py
@@ -1,17 +1,28 @@
 from montecarlo import monte_carlo
-from visualization import show_monomers
+from visualization import show_monomers, create_plotter
 from grid_creator import create_grid
 import time
 
-#Oppgave 1f)
+
+# Oppgave 1f)
 def simulate_monomers(N, M, N_s, T):
     grid = create_grid(N, M)
     grid, epsilon = monte_carlo(grid, N_s, M, T)
     show_monomers(grid, M)
 
+
+# Remember to turn off plots in tool window in Preferences > Tools > Python Scientific
+# to see the animation.
+def simulate_monomers_with_visualizations(N, M, N_s, T):
+    grid = create_grid(N, M)
+    _, _ = monte_carlo(grid, N_s, M, T, create_plotter())
+
+
+# Remember that old code is still stored in git. If you want to see the chanegs to this document,
+# right click the this file's tab, then Git > Show History
+def main():
+    simulate_monomers_with_visualizations(15, 25, 50000, 500)
+
+
 if __name__ == "__main__":
-
-
-    t0=time.time()
-    simulate_monomers(15, 25, 50000, 500)
-    print(time.time()-t0)
+    main()
