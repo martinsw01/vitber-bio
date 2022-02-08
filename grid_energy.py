2419112674c1aa3b6aa9763a4d32fdfb3236ee5e
martinsw01 <martin.s.winther@gmail.com>
2022-02-08 17:14:02 +0100

    #5 Fixed bug in energy

--- grid_energy.py
+++ grid_energy.py
@@ -1,11 +1,12 @@
 import numpy as np
+
 from grid import neighbor_coordinates
 
 ε_r = 78  # [1] Relative permittivity
-ε_0 = 8.85418782e-12  # vacuum permittivity[m^-3 kg^-1 s^4 A^2]
-a = 23e-6  # Distance between grid points (23 microns)
+ε_0 = 8.85418782e-12  # [m^-3 kg^-1 s^4 A^2] vacuum permittivity
+a = 23e-6  # [m] Distance between grid points (23 microns)
 e = 1.60217662e-19  # [C]
-α = e ** 2 / (4 * np.pi * ε_r * ε_0) * a ** 2
+α = e ** 2 / (4 * np.pi * ε_r * ε_0 * a ** 2)
 
 
 # 1 d)
@@ -16,10 +17,9 @@ def calc_energy(grid):
 
     for monomer_index in zip(*monomer_indices):
         monomer = grid[monomer_index]
-        for i, j in neighbor_coordinates(N, *monomer_index):
-            neighbour_monomer = grid[i, j]
+        for neighbor_coord in neighbor_coordinates(N, *monomer_index):
+            neighbour_monomer = grid[neighbor_coord]
             if neighbour_monomer != monomer and neighbour_monomer != 0:  # i.e. not the same type or solvent
-                some_energy = np.sign(monomer) * np.sign(neighbour_monomer)
-                relative_energy += some_energy
+                relative_energy += np.sign(monomer) * np.sign(neighbour_monomer)
 
-    return relative_energy * α**2 / 2
+    return relative_energy * α / 2
