7d159f6f03998affe0ac208562df4a2dc1ea3bc1
Trygvus <45048887+Trygvus@users.noreply.github.com>
2022-02-08 11:13:40 +0100

    #7 laget ferdig simulations, men ikke sikker på om alt er riktig

--- simulations.py
+++ simulations.py
@@ -1,9 +1,17 @@
 from montecarlo import monte_carlo
 from visualization import show_monomers
 from grid_creator import create_grid
-
+import time
 
 #Oppgave 1f)
 def simulate_monomers(N, M, N_s, T):
     grid = create_grid(N, M)
-    monte_carlo(grid, N_s, M, T)
+    grid, epsilon = monte_carlo(grid, N_s, M, T)
+    show_monomers(grid, M)
+
+if __name__ == "__main__":
+
+
+    t0=time.time()
+    simulate_monomers(15, 25, 50000, 500)
+    print(time.time()-t0)
