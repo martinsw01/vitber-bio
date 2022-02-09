a114823d7091f565fbc74dc2abbd1f1865544613
Trygvus <45048887+Trygvus@users.noreply.github.com>
2022-02-09 15:08:37 +0100

    close #14 finished medium_flexibility_move with numba support

--- move.py
+++ move.py
@@ -31,7 +31,7 @@ def rigid_move(grid, polymer, direction):
 
 
 #2 e)
-# @njit
+@njit
 def medium_flexibility_move(grid, polymer, direction):
     N = len(grid)
     monomer_positions = np.array([[i, j] for i in range(N) for j in range(N) if grid[i, j] == polymer])
@@ -43,11 +43,23 @@ def medium_flexibility_move(grid, polymer, direction):
     new_grid = np.copy(grid)
     for coordinate in monomer_positions:
         value_at_new_position = grid[(coordinate[0]+direction[0]) % N, (coordinate[1] + direction[1]) %N]
+
         if value_at_new_position != 0 and value_at_new_position != polymer:
-            for coord in monomer_positions:
+            for ind, coord in enumerate(monomer_positions):
                 if direction[0]:
                     if coord[1] == coordinate[1]:
-                        # index_of_frozen_monomer = np.array([])
+                        attempted_new_positions[ind] -= direction
+                else:
+                    if coord[0] == coordinate[0]:
+                        attempted_new_positions[ind] -= direction
+    for i, j in monomer_positions:
+        new_grid[i,j] = 0
+    for i, j in attempted_new_positions:
+        new_grid[i, j] = polymer
+    return new_grid
+
+
+"""" index_of_frozen_monomer = np.array([])
                         index_of_frozen_monomer = np.where(np.any(moving_monomers-coord, axis=1)==False)[0][0]
                         moving_monomers = np.delete(moving_monomers, index_of_frozen_monomer, axis=0)
                         # moving_monomers = remove_coordinate_from_array_of_coordinates(moving_monomers, coord)
@@ -60,7 +72,7 @@ def medium_flexibility_move(grid, polymer, direction):
         new_grid[monomer[0], monomer[1]] = 0
     for monomer in moving_monomers:
         new_grid[(monomer[0] + direction[0]) % N, (monomer[1] + direction[1]) % N] = polymer
-    return new_grid
+    return new_grid"""
 
 
 """@njit
