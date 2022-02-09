4678389481a2315ddeec36849b4951cd8aa9ccd7
Trygvus <45048887+Trygvus@users.noreply.github.com>
2022-02-09 15:10:00 +0100

    close #14 cleaned medium_flexibility_move

--- move.py
+++ move.py
@@ -39,7 +39,6 @@ def medium_flexibility_move(grid, polymer, direction):
     if is_legal_rigid_move(grid, polymer, attempted_new_positions):
         return rigid_move(grid, polymer, direction)
 
-    moving_monomers = np.copy(monomer_positions)
     new_grid = np.copy(grid)
     for coordinate in monomer_positions:
         value_at_new_position = grid[(coordinate[0]+direction[0]) % N, (coordinate[1] + direction[1]) %N]
@@ -53,30 +52,8 @@ def medium_flexibility_move(grid, polymer, direction):
                     if coord[0] == coordinate[0]:
                         attempted_new_positions[ind] -= direction
     for i, j in monomer_positions:
-        new_grid[i,j] = 0
+        new_grid[i, j] = 0
     for i, j in attempted_new_positions:
         new_grid[i, j] = polymer
     return new_grid
 
-
-"""" index_of_frozen_monomer = np.array([])
-                        index_of_frozen_monomer = np.where(np.any(moving_monomers-coord, axis=1)==False)[0][0]
-                        moving_monomers = np.delete(moving_monomers, index_of_frozen_monomer, axis=0)
-                        # moving_monomers = remove_coordinate_from_array_of_coordinates(moving_monomers, coord)
-                else:
-                    if coord[0] == coordinate[0]:
-                        index_of_frozen_monomer = np.where(np.any(moving_monomers - coord, axis=1) == False)[0][0]
-                        moving_monomers = np.delete(moving_monomers, index_of_frozen_monomer, axis=0)
-                        #moving_monomers = remove_coordinate_from_array_of_coordinates(moving_monomers, coord)
-    for monomer in moving_monomers:
-        new_grid[monomer[0], monomer[1]] = 0
-    for monomer in moving_monomers:
-        new_grid[(monomer[0] + direction[0]) % N, (monomer[1] + direction[1]) % N] = polymer
-    return new_grid"""
-
-
-"""@njit
-def remove_coordinate_from_array_of_coordinates(array, coordinate):
-    for ind, val in enumerate(array):
-        if val == coordinate:
-            return np.delete(array, ind, axis=0)"""
