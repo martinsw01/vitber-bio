af9763db54795df8686e5b87223be562d6184d87
Trygvus <45048887+Trygvus@users.noreply.github.com>
2022-02-09 14:57:54 +0100

    #14 finished medium_flexibility_move, but missing numba support

--- move.py
+++ move.py
@@ -31,36 +31,40 @@ def rigid_move(grid, polymer, direction):
 
 
 #2 e)
-
+# @njit
 def medium_flexibility_move(grid, polymer, direction):
     N = len(grid)
-    monomer_positions = np.array(np.where(grid == polymer)).T
-    attempted_rigid_move = tuple((monomer_positions + direction).T % N)
-    if is_legal_rigid_move(grid, polymer, attempted_rigid_move):
+    monomer_positions = np.array([[i, j] for i in range(N) for j in range(N) if grid[i, j] == polymer])
+    attempted_new_positions = (monomer_positions + direction) % N
+    if is_legal_rigid_move(grid, polymer, attempted_new_positions):
         return rigid_move(grid, polymer, direction)
-    print("Is not legal rigid move")
-    frozen_monomers = np.array([[-1, -1]])
+
+    moving_monomers = np.copy(monomer_positions)
     new_grid = np.copy(grid)
     for coordinate in monomer_positions:
-        print(coordinate)
         value_at_new_position = grid[(coordinate[0]+direction[0]) % N, (coordinate[1] + direction[1]) %N]
         if value_at_new_position != 0 and value_at_new_position != polymer:
             for coord in monomer_positions:
                 if direction[0]:
                     if coord[1] == coordinate[1]:
-                        frozen_monomers = np.append(frozen_monomers, [coord], axis=0)
+                        # index_of_frozen_monomer = np.array([])
+                        index_of_frozen_monomer = np.where(np.any(moving_monomers-coord, axis=1)==False)[0][0]
+                        moving_monomers = np.delete(moving_monomers, index_of_frozen_monomer, axis=0)
+                        # moving_monomers = remove_coordinate_from_array_of_coordinates(moving_monomers, coord)
                 else:
                     if coord[0] == coordinate[0]:
-                        frozen_monomers = np.append(frozen_monomers, [coord], axis=0)
-    moving_monomers = np.array([])
-    print(frozen_monomers)
-    for coordinate in monomer_positions:
-        print(coordinate)
-        if coordinate not in frozen_monomers:
-            print("Hei")
-            new_grid[coordinate[0], coordinate[1]] = 0
-            moving_monomers = np.append(moving_monomers, coordinate)
+                        index_of_frozen_monomer = np.where(np.any(moving_monomers - coord, axis=1) == False)[0][0]
+                        moving_monomers = np.delete(moving_monomers, index_of_frozen_monomer, axis=0)
+                        #moving_monomers = remove_coordinate_from_array_of_coordinates(moving_monomers, coord)
+    for monomer in moving_monomers:
+        new_grid[monomer[0], monomer[1]] = 0
     for monomer in moving_monomers:
         new_grid[(monomer[0] + direction[0]) % N, (monomer[1] + direction[1]) % N] = polymer
     return new_grid
 
+
+"""@njit
+def remove_coordinate_from_array_of_coordinates(array, coordinate):
+    for ind, val in enumerate(array):
+        if val == coordinate:
+            return np.delete(array, ind, axis=0)"""
