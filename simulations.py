2771d8b1f91f260bd7223efe0ad431c8dc773b4d
martinsw01 <martin.s.winther@gmail.com>
2022-02-09 14:01:33 +0100

    Closes #13 2 d)

--- simulations.py
+++ simulations.py
@@ -1,7 +1,9 @@
 import numpy as np
+from numba import njit
 
-from grid_creator import create_grid
+from grid_creator import create_grid, create_polymer_grid
 from montecarlo import monte_carlo
+from move import rigid_move
 from visualization import plot_energy_and_grid, create_plotter, plot_mean_cluster_sizes
 
 
@@ -45,6 +47,23 @@ def sim_mean_cluster_size():
 
     mean_cluster_sizes = [mean_cluster_size for _, _, mean_cluster_size in results]
     plot_mean_cluster_sizes(mean_cluster_sizes, temperatures)
+
+
+@njit
+def allways_false(*args): return False
+
+
+# 2 d)
+def simulation_with_polymers():
+    N = 30
+    L = 13
+    M = 6
+    T = 200
+    N_s = 30_000
+    grid = create_polymer_grid(N, M, L)
+    final_grid, energy, _ = monte_carlo(grid, N_s, M, T, move_polymer=rigid_move, is_illegal_move=allways_false)
+    plt.pcolormesh(final_grid[::-1, ])
+    plt.show()
 
 
 def main():
